e9918feec84f89a3ab3f35d415de3f6341ee2cfe
Kevin Johr <kevin.johr@posteo.de>
2023-06-25 22:22:00 +0200

    #9  add view for editcreate
    implement new view and add this to ContentViewController
    - implement also the form to create new tool

--- Controllers/ContentViewController.cs
+++ Controllers/ContentViewController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ComnuyWebWithAPI.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ComnuyWebWithAPI.Controllers
 {
     public class ContentViewController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ContentViewController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -14,6 +23,20 @@ namespace ComnuyWebWithAPI.Controllers
         public IActionResult MyContent()
         {
             return View();
+        }
+
+        [Authorize]
+        public IActionResult CreateOrEditTool()
+        {
+            var toolGroups = GetToolGroups();
+            ViewBag.ToolGroups = new SelectList(toolGroups);
+            return View();
+        }
+
+        private List<string> GetToolGroups()
+        {
+            var toolGroups = _context.ToolGroups.Select(t => t.Name).ToList();
+            return toolGroups;
         }
     }
 }
