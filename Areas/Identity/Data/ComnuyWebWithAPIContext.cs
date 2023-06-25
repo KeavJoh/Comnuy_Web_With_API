2af7e00d98090e970e9a05016ac50041340afca6
Kevin Johr <kevin.johr@posteo.de>
2023-06-25 14:11:59 +0200

    #1  add login and registration scaffolding

---
+++ Areas/Identity/Data/ComnuyWebWithAPIContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComnuyWebWithAPI.Data;
+
+public class ComnuyWebWithAPIContext : IdentityDbContext<IdentityUser>
+{
+    public ComnuyWebWithAPIContext(DbContextOptions<ComnuyWebWithAPIContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
