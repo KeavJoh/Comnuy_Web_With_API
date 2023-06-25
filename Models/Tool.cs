9d5fa92944168af2408e6cb9f30b74a6ce6df87d
Kevin Johr <kevin.johr@posteo.de>
2023-06-25 14:30:58 +0200

    #2  add Tool object

---
+++ Models/Tool.cs
@@ -0,0 +1,16 @@
+﻿namespace ComnuyWebWithAPI.Models
+{
+    public class Tool
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? Description { get; set; }
+        public string? WebAddress { get; set; }
+        public string? Picture { get; set; }
+        public string? Owner { get; set; }
+        public string? LastChangeUser { get; set; }
+        public DateTime LastChangesDate { get; set; }
+        public int ToolGroup { get; set; }
+    }
+}
