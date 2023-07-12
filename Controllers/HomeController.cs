8e3ba609859696606c763c460d8c007e2087f8d5
Kevin Johr <kevin.johr@posteo.de>
2023-07-12 11:32:49 +0200

    #14 add partial view for newest tools

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace ComnuyWebWithAPI.Controllers
         public IActionResult Index()
         {
             List<Tool> toolsFromDb = _context.Tools.ToList();
-            List<Tool> toolOrderByDateFromDb = _context.Tools.OrderBy(x => x.LastChangesDate).Take(3).ToList();
+            List<Tool> toolOrderByDateFromDb = _context.Tools.OrderByDescending(x => x.LastChangesDate).Take(3).ToList();
             Random random = new();
             var randomTools = toolsFromDb.OrderBy(x => random.Next()).Take(3).ToList();
 
